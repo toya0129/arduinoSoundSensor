a5a9c15279ae6d5ee77d8e52cb7ef3980ca410c4
toya0129 <citu15059@g.nihon-u.ac.jp>
2018-09-17 18:00:12 +0900

    一時保存

--- arduinoSoundSensor.ino
+++ arduinoSoundSensor.ino
@@ -5,77 +5,60 @@
 #include <FloatDefine.h>
 #include <RunningStatistics.h>
 
-// sensor
-#define data 5
-#define clk 4
+// Mic Sensor
+#define mic 13
+#define LR 10
+
+// Clock Sensor
+#define clkA 5
+#define clkB 4
+#define clkC 3
+#define clkST 6
 
 // sensor state
 float Cstate = 0;
-
-//test
-const int maxloop = 100;
-const int maxdac = 4095;
-const int maxrate = 2000 / maxloop;
-const int maxset = maxdac / 2;
-const int outloop = 10000 / maxloop;
-int count = 0;
-long c,b;
-
-float highdata;
-float lowdata;
-float hz = 20000.0;
+float a = 0;
+float c = 0;
+float low = 0;
+float hz = 2000.0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
 
-  // sensor set
-  pinMode(data,INPUT);
-  pinMode(clk,OUTPUT);
+  PinSet();
+  SensorSet();
+
+}
+
+void PinSet(){
+  pinMode(mic,INPUT);
+  pinMode(LR,OUTPUT);
+
+  pinMode(clkA,OUTPUT);
+  pinMode(clkB,OUTPUT);
+  pinMode(clkC,OUTPUT);
+  pinMode(clkST,OUTPUT);
+}
+
+void SensorSet(){
+//  digitalWrite(LR,HIGH);
+  digitalWrite(clkA,HIGH);
+  digitalWrite(clkB,LOW);
+  digitalWrite(clkC,LOW);
+  digitalWrite(clkST,HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-//  while(1){
-//
-//  for(int i= 0;i< outloop;i++){
-//    count = 0;
-//    for(int a = 0;a< maxloop;a++){
-//      digitalWrite(clk,LOW);
-//      c++;
-//      b++;
-//      if(digitalRead(data) == HIGH){
-//        count++;
-//      }else{
-//        count--;
-//      }
-//      if(a < maxloop - 1){
-//        digitalWrite(clk,HIGH);
-//        digitalWrite(clk,HIGH);
-//      }
-//    }
-//    count = count * maxrate + maxset;
-//  }
-//  Serial.println(count);
-//  }
-
   
+  a = digitalRead(mic);
+  //LowPassFilter(a);
 
-//  digitalWrite(clk,LOW);
+  Serial.println(a);
+  //Serial.println(Cstate);
 
-  
-
-  Cstate = analogRead(data);
-  //LowPassFilter(Cstate);
-  Serial.println(Cstate);
-  //delay(100);
 }
-
-//void ReadVoiceSensor(){
-//  Cstate = analogRead(Csen);
-//  data = (float)Cstate;
-//}
 
 void LowPassFilter(float in){
   FilterOnePole lowpassFilter( LOWPASS, hz );
